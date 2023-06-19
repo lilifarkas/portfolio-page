cec9ba019d44df74cf1429dbe5ef7f89fd3e8b72
lili.farkas <lili.farkas14@gmail.com>
2023-06-19 19:04:16 +0200

    Create the about section

---
+++ portofilo/src/Components/About/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+    return (
+        <section id="about" className="about">
+            <h2>About Me</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices, nisi ac mollis bibendum, velit libero malesuada mauris, ac tempus quam purus ut enim.</p>
+            {/* Add more content about yourself */}
+        </section>
+    );
+}
+
+export default About;
