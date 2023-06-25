1a2200e487a735023c73c037a91c537873e2abd2
lili.farkas <lili.farkas14@gmail.com>
2023-06-25 21:35:29 +0200

    Add footer content and design

--- portofilo/src/Components/Footer/Footer.jsx
+++ portofilo/src/Components/Footer/Footer.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
+    const handleEmailClick = () => {
+        window.location.href = 'mailto:lili.farkas14@gmail.com';
+    };
+    
     return (
-        <footer className="footer">
-            <p>&copy; 2023 Your Name</p>
+        <footer className="footer d-flex flex-row justify-content-center gap-5 align-items-center">
+            <p className="rainbow-text contacts">Contacts:</p>
+            <p onClick={handleEmailClick} className="email mt-3">lili.farkas14@gmail.com</p>
+            
+            <a href="https://www.linkedin.com/in/lilifarkas" target="_blank" rel="noopener noreferrer"
+            className="social-icons">
+                <FontAwesomeIcon icon={faLinkedin} size="2x" color="white"/>
+            </a>
+            <a href="https://www.facebook.com/lili.farkas.31/" target="_blank" rel="noopener noreferrer"
+               className="social-icons">
+                <FontAwesomeIcon icon={faFacebook} size="2x" color="white"/>
+            </a>
+            <a href="https://github.com/lilifarkas" target="_blank" rel="noopener noreferrer" className="social-icons">
+                <FontAwesomeIcon icon={faGithub} size="2x" color="white" />
+            </a>
         </footer>
     );
 }
