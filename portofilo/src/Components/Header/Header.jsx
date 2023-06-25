1793ffb2c4596bac39d9b472afb936da314b55c3
lili.farkas <lili.farkas14@gmail.com>
2023-06-25 21:37:38 +0200

    Add icons to the Header

--- portofilo/src/Components/Header/Header.jsx
+++ portofilo/src/Components/Header/Header.jsx
@@ -3,6 +3,8 @@ import './Header.css';
 import profilePicture from '../images/githubpic-removebg-preview.png';
 import { useSpring, animated } from 'react-spring';
 import 'bootstrap/dist/css/bootstrap.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons";
 
 function Header({ isDarkMode, onModeToggle }) {
 
@@ -28,18 +30,15 @@ function Header({ isDarkMode, onModeToggle }) {
                     >
                         {isDarkMode ? 'Dark Light' : 'Light Dark'}
                     </button>
-                    {/*<DayNightToggle*/}
-                    {/*    onChange={onModeToggle}*/}
-                    {/*    checked={isDarkMode}*/}
-                    {/*    className=""*/}
-                    {/*/>*/}
-                    {/*<input*/}
-                    {/*    type="checkbox"*/}
-                    {/*    id="toggle"*/}
-                    {/*    checked={isDarkMode}*/}
-                    {/*    onChange={onModeToggle}*/}
-                    {/*/>*/}
-                    {/*<label htmlFor="toggle" className="toggle-label" />*/}
+                </div>
+                <div className="links mt-5 d-flex flex-row gap-3">
+                    <a href="https://www.linkedin.com/in/lilifarkas" target="_blank" rel="noopener noreferrer"
+                       className="social-icons">
+                        <FontAwesomeIcon icon={faLinkedin} size="2x" color="#476098"/>
+                    </a>
+                    <a href="https://github.com/lilifarkas" target="_blank" rel="noopener noreferrer" className="social-icons">
+                        <FontAwesomeIcon icon={faGithub} size="2x" color="#476098"/>
+                    </a>
                 </div>
             </div>
             <div className="header-content">
@@ -49,8 +48,11 @@ function Header({ isDarkMode, onModeToggle }) {
                         Full-Stack Developer
                     </div>
                     <br /><br />
-                    <p className="intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices, nisi ac mollis bibendum,
-                        ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <p className="intro">Along with my studies in health promotion, my extensive experience 
+                        in both individual and collaborative work settings has given me a unique perspective that 
+                        I bring to software development, to meet the specific requirements of each project. 
+                        Experienced in connecting front-end design with backend infrastructure and possess a 
+                        strong grasp of OOP concepts and techniques.</p>
                     <animated.button
                         className="cv-button gradient-text"
                         style={buttonAnimation}
