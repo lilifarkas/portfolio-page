63c2e83ef1507ac2d0d3aaed3408e373308c8617
lili.farkas <lili.farkas14@gmail.com>
2023-06-25 21:38:18 +0200

    Refactor the App.js and add content

--- portofilo/src/App.js
+++ portofilo/src/App.js
@@ -5,6 +5,7 @@ import About from './Components/About/About';
 import ProjectCard from './Components/ProjectCards/ProjectCard';
 import Footer from './Components/Footer/Footer';
 import Technologies from './Components/Technologies/Technologies';
+import ProjectOnePic from "../src/Components/images/ProjectPics/1.PNG"
 
 function App() {
     const [isDarkMode, setIsDarkMode] = useState(false);
@@ -16,35 +17,58 @@ function App() {
     return (
         <div className={`App ${isDarkMode ? 'dark-mode' : ''}`}>
             <Header isDarkMode={isDarkMode} onModeToggle={handleModeToggle} />
-            <div className="mt-5 d-flex justify-content-center align-items-center">
-                <section className="projects d-flex flex-column justify-content-center align-items-center w-75">
-                    <h2 className="align-self-start">Projects</h2>
-                    <div className="project-cards">
-                        <ProjectCard
-                            title="Project 1"
-                            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices, nisi ac mollis bibendum."
-                            imageSrc="project1.jpg"
-                            githubLink="https://github.com/example/project1"
-                        />
-                        <ProjectCard
-                            title="Project 2"
-                            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices, nisi ac mollis bibendum."
-                            imageSrc="project2.jpg"
-                            githubLink="https://github.com/example/project2"
-                        />
-                        <ProjectCard
-                            title="Project 3"
-                            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices, nisi ac mollis bibendum."
-                            imageSrc="project2.jpg"
-                            githubLink="https://github.com/example/project2"
-                        />
-                        <ProjectCard
-                            title="Project 4"
-                            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices, nisi ac mollis bibendum."
-                            imageSrc="project2.jpg"
-                            githubLink="https://github.com/example/project2"
-                        />
-                        {/* Add more project cards here */}
+            <div className="mt-5 mb-5 d-flex" id="projects">
+                <section className="d-flex justify-content-center align-items-center flex-column">
+                    <h2 className="title-projects">Projects</h2>
+                    <div className="d-flex flex-row justify-content-center project-card-container gap-4 w-75">
+                        <div className="project-card d-flex flex-row justify-content-center align-items-center mt-4">
+                            <ProjectCard
+                                title="Health Center Page"
+                                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices, nisi ac mollis bibendum."
+                                imageSrc={ProjectOnePic}
+                                githubLink="https://github.com/lilifarkas/health-center-page"
+                            />
+                        </div>
+                        <div className="project-card d-flex flex-row justify-content-center align-items-center mt-4 w-25">
+                            <ProjectCard
+                                title="Mental Health Journal Application"
+                                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices, nisi ac mollis bibendum."
+                                imageSrc="project2.jpg"
+                                githubLink="https://github.com/lilifarkas/mental-health-journal"
+                            />
+                        </div>
+                        <div className="project-card d-flex flex-row justify-content-center align-items-center mt-4 w-25">
+                            <ProjectCard
+                                title="Rick And Morty"
+                                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices, nisi ac mollis bibendum."
+                                imageSrc="project2.jpg"
+                                githubLink="https://github.com/lilifarkas/rick-and-morty"
+                            />
+                        </div>
+                        <div className="project-card d-flex flex-row justify-content-center align-items-center mt-4 w-25">
+                            <ProjectCard
+                                title="Portfolio Page"
+                                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices, nisi ac mollis bibendum."
+                                imageSrc="project2.jpg"
+                                githubLink="https://github.com/lilifarkas/portfolio-page"
+                            />
+                        </div>
+                        <div className="project-card d-flex flex-row justify-content-center align-items-center mt-4 w-25">
+                            <ProjectCard
+                                title="Employees data"
+                                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices, nisi ac mollis bibendum."
+                                imageSrc="project2.jpg"
+                                githubLink="https://github.com/lilifarkas/employees-data"
+                            />
+                        </div>
+                        <div className="project-card d-flex flex-row justify-content-center align-items-center mt-4 w-25">
+                            <ProjectCard
+                                title="Mars exploration"
+                                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices, nisi ac mollis bibendum."
+                                imageSrc="project2.jpg"
+                                githubLink="https://github.com/lilifarkas/mars-exploration"
+                            />
+                        </div>
                     </div>
                 </section>
             </div>
