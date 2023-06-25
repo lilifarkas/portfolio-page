96d0cfabc9172b4a8304cf3a1df8cf0879f4ad59
lili.farkas <lili.farkas14@gmail.com>
2023-06-25 21:36:50 +0200

    Add another link to the project cards and refactor the design

--- portofilo/src/Components/ProjectCards/ProjectCard.jsx
+++ portofilo/src/Components/ProjectCards/ProjectCard.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import './ProjectCard.css';
 
+
 function ProjectCard({ title, description, imageSrc, githubLink }) {
     return (
-        <div className="project-card">
+        <div className="project-card ">
             <div className="card-content">
                 <img src={imageSrc} alt={title} />
                 <div className="project-info">
                     <h3>{title}</h3>
                     <p>{description}</p>
-                    <a href={githubLink} target="_blank" rel="noopener noreferrer">
-                        GitHub
-                    </a>
+                    <div className="d-flex flex-row justify-content-center align-items-center gap-4">
+                        <a href={githubLink} target="_blank" rel="noopener noreferrer">
+                            Check on GitHub
+                        </a>
+                        <a href={githubLink} target="_blank" rel="noopener noreferrer">
+                            Live preview
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
