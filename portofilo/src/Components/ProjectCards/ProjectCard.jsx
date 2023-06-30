9b0fafad7ef3dbd744238ae4ca0a97a41d19cc4e
lili.farkas <lili.farkas14@gmail.com>
2023-06-30 13:44:03 +0200

    Add preview link to the project cards

--- portofilo/src/Components/ProjectCards/ProjectCard.jsx
+++ portofilo/src/Components/ProjectCards/ProjectCard.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import './ProjectCard.css';
 
 
-function ProjectCard({ title, description, imageSrc, githubLink }) {
+function ProjectCard({ title, description, imageSrc, githubLink, previewLink}) {
     return (
         <div className="project-card ">
             <div className="card-content">
                 <img src={imageSrc} alt={title} />
                 <div className="project-info">
                     <h3>{title}</h3>
-                    <p>{description}</p>
+                    <p className="project-description">{description}</p>
                     <div className="d-flex flex-row justify-content-center align-items-center gap-4">
                         <a href={githubLink} target="_blank" rel="noopener noreferrer">
                             Check on GitHub
                         </a>
-                        <a href={githubLink} target="_blank" rel="noopener noreferrer">
+                        <a href={previewLink} target="_blank" rel="noopener noreferrer">
                             Live preview
                         </a>
                     </div>
