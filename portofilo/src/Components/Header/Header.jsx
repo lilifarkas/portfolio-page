4f28aadd658f24f2f42c6226184d1c89a37c2876
lili.farkas <lili.farkas14@gmail.com>
2023-06-30 13:42:20 +0200

    Add dark mode toggle button, hamburger menu, and add content to the download CV functionality

--- portofilo/src/Components/Header/Header.jsx
+++ portofilo/src/Components/Header/Header.jsx
@@ -5,39 +5,63 @@ import { useSpring, animated } from 'react-spring';
 import 'bootstrap/dist/css/bootstrap.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons";
+import { Sling as Hamburger } from 'hamburger-react'
+import { DarkModeToggle } from '@anatoliygatt/dark-mode-toggle';
+import CV from '../Cv/lili_farkas__FULLSTACK-10M-DAYTIME (2).pdf'
 
 function Header({ isDarkMode, onModeToggle }) {
-
+    const [mode, setMode] = useState('dark');
     const [isHovered, setIsHovered] = useState(false);
+    const [hamburgerOpen, setHamburgerOpen] = useState(false);
+    
+    const toggleHamburger = () =>{
+        setHamburgerOpen(!hamburgerOpen);
+    }
 
     const buttonAnimation = useSpring({
         transform: isHovered ? 'scale(1.1)' : 'scale(1)',
         backgroundColor: isHovered ? 'rgb(17, 169, 199)' : 'rgb(142, 91, 210)',
     });
+
+    const handleModeChange = (mode) => {
+        setMode(mode);
+        onModeToggle(); 
+    };
+
+    const handleDownloadCV = () => {
+        const link = document.createElement('a');
+        link.href = CV;
+        link.download = 'lili_farkas_FULLSTACK-10M-DAYTIME';
+        link.click();
+    };
     
     return (
+        <>
         <header className={`header ${isDarkMode ? 'dark-mode' : ''}`}>
             <div className="navbar-profile">
-                <nav className="navbar-nav d-flex flex-row mt-5">
+                <div className="hamburger">
+                    <Hamburger size={20} onToggle={toggleHamburger}/>
+                </div>
+                <nav className={`navbar-nav d-flex flex-row mt-5 ${hamburgerOpen ? 'show' : 'dontShow'}`}>
                     <a href="#about">About</a>
                     <a href="#projects">Projects</a>
                     <a href="#technologies">Technologies</a>
                 </nav>
                 <div className="mode-toggle mt-5 ">
-                    <button
-                        className={`toggle-button ${isDarkMode ? 'dark' : 'light'}`}
-                        onClick={onModeToggle}
-                    >
-                        {isDarkMode ? 'Dark Light' : 'Light Dark'}
-                    </button>
+                    <DarkModeToggle
+                        mode={mode}
+                        size="sm"
+                        onChange={handleModeChange}
+                        
+                    />
                 </div>
                 <div className="links mt-5 d-flex flex-row gap-3">
                     <a href="https://www.linkedin.com/in/lilifarkas" target="_blank" rel="noopener noreferrer"
                        className="social-icons">
-                        <FontAwesomeIcon icon={faLinkedin} size="2x" color="#476098"/>
+                        <FontAwesomeIcon icon={faLinkedin} size="2x" color="#56A8C4"/>
                     </a>
                     <a href="https://github.com/lilifarkas" target="_blank" rel="noopener noreferrer" className="social-icons">
-                        <FontAwesomeIcon icon={faGithub} size="2x" color="#476098"/>
+                        <FontAwesomeIcon icon={faGithub} size="2x" color="#56A8C4"/>
                     </a>
                 </div>
             </div>
@@ -58,6 +82,7 @@ function Header({ isDarkMode, onModeToggle }) {
                         style={buttonAnimation}
                         onMouseEnter={() => setIsHovered(true)}
                         onMouseLeave={() => setIsHovered(false)}
+                        onClick={handleDownloadCV}
                     >
                         Download CV
                     </animated.button>
@@ -67,6 +92,7 @@ function Header({ isDarkMode, onModeToggle }) {
                 </div>
             </div>
         </header>
+        </>
     );
 }
 
