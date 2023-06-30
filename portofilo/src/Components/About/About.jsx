7b1f287bc7f045b5e21511cf4020276611c7613a
lili.farkas <lili.farkas14@gmail.com>
2023-06-30 13:44:34 +0200

    Add content to the about section and design it

--- portofilo/src/Components/About/About.jsx
+++ portofilo/src/Components/About/About.jsx
@@ -3,10 +3,60 @@ import './About.css';
 
 function About() {
     return (
-        <section id="about" className="about">
-            <h2>About Me</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices, nisi ac mollis bibendum, velit libero malesuada mauris, ac tempus quam purus ut enim.</p>
-            {/* Add more content about yourself */}
+        <section className="experience">
+            <h2 className="section-title">About me</h2>
+            <div className="experience-item">
+                <div className="experience-year">
+                    <span className="year">2018</span>
+                    <div>
+                        <div className="dot"></div>
+                        <div className="line"></div>
+                    </div>
+                </div>
+                <div className="experience-content">
+                    <h3>University of Szeged</h3>
+                    <p>Recreation and Health Promotion Manager - Bachelor's Degree</p>
+                </div>
+            </div>
+            <div className="experience-item">
+                <div className="experience-year">
+                    <span className="year">2019- 2021</span>
+                    <div>
+                        <div className="dot"></div>
+                        <div className="line"></div>
+                    </div>
+                </div>
+                <div className="experience-content">
+                    <h3>Országos Kórházi Főigazgatóság</h3>
+                    <p>Reflexologist and Ear acupuncture addictologist - Certificate</p>
+                </div>
+            </div>
+            <div className="experience-item">
+                <div className="experience-year">
+                    <span className="year">2019- 2022</span>
+                    <div>
+                        <div className="dot"></div>
+                        <div className="line"></div>
+                    </div>
+                </div>
+                <div className="experience-content">
+                    <h3>M-DENS Kft.</h3>
+                    <p>Medical Assistant</p>
+                </div>
+            </div>
+            <div className="experience-item">
+                <div className="experience-year">
+                    <span className="year">2022- 2023</span>
+                    <div>
+                        <div className="dot"></div>
+                        <div className="line"></div>
+                    </div>
+                </div>
+                <div className="experience-content">
+                    <h3>CODECOOL</h3>
+                    <p>Full Stack Developer Student</p>
+                </div>
+            </div>
         </section>
     );
 }
