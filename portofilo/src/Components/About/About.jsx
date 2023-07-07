15c90ec5b31dcef5077b1b390a68f1fc83be0fb8
lili.farkas <lili.farkas14@gmail.com>
2023-07-07 19:35:30 +0200

    Add design to the About section and make it fully responsive

--- portofilo/src/Components/About/About.jsx
+++ portofilo/src/Components/About/About.jsx
@@ -3,32 +3,19 @@ import './About.css';
 
 function About() {
     return (
-        <section className="experience">
+        <section className="experience" id="about">
             <h2 className="section-title">About me</h2>
             <div className="experience-item">
                 <div className="experience-year">
-                    <span className="year">2018</span>
+                    <span className="year">2022- 2023</span>
                     <div>
                         <div className="dot"></div>
                         <div className="line"></div>
                     </div>
                 </div>
                 <div className="experience-content">
-                    <h3>University of Szeged</h3>
-                    <p>Recreation and Health Promotion Manager - Bachelor's Degree</p>
-                </div>
-            </div>
-            <div className="experience-item">
-                <div className="experience-year">
-                    <span className="year">2019- 2021</span>
-                    <div>
-                        <div className="dot"></div>
-                        <div className="line"></div>
-                    </div>
-                </div>
-                <div className="experience-content">
-                    <h3>Országos Kórházi Főigazgatóság</h3>
-                    <p>Reflexologist and Ear acupuncture addictologist - Certificate</p>
+                    <h3>CODECOOL</h3>
+                    <p>Full Stack Developer Student</p>
                 </div>
             </div>
             <div className="experience-item">
@@ -46,17 +33,33 @@ function About() {
             </div>
             <div className="experience-item">
                 <div className="experience-year">
-                    <span className="year">2022- 2023</span>
+                    <span className="year">2019- 2021</span>
                     <div>
                         <div className="dot"></div>
                         <div className="line"></div>
                     </div>
                 </div>
                 <div className="experience-content">
-                    <h3>CODECOOL</h3>
-                    <p>Full Stack Developer Student</p>
+                    <h3>Országos Kórházi Főigazgatóság</h3>
+                    <p>Reflexologist and Ear acupuncture addictologist - Certificate</p>
                 </div>
             </div>
+            <div className="experience-item">
+                <div className="experience-year">
+                    <span className="year">2018</span>
+                    <div>
+                        <div className="dot"></div>
+                        <div className="line"></div>
+                    </div>
+                </div>
+                <div className="experience-content">
+                    <h3>University of Szeged</h3>
+                    <p>Recreation and Health Promotion Manager - Bachelor's Degree</p>
+                </div>
+            </div>
+            
+            
+            
         </section>
     );
 }
