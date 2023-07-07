cdae488d28949e63e401db1bfe616bd5493ed9b1
lili.farkas <lili.farkas14@gmail.com>
2023-07-07 21:03:18 +0200

    Refactor the Header and make it fully responsive

--- portofilo/src/Components/Header/Header.jsx
+++ portofilo/src/Components/Header/Header.jsx
@@ -37,17 +37,20 @@ function Header({ isDarkMode, onModeToggle }) {
     
     return (
         <>
-        <header className={`header ${isDarkMode ? 'dark-mode' : ''}`}>
+        <header className={`header ${isDarkMode ? 'dark-mode' : ''} `}>
             <div className="navbar-profile">
-                <div className="hamburger">
-                    <Hamburger size={20} onToggle={toggleHamburger}/>
-                </div>
-                <nav className={`navbar-nav d-flex flex-row mt-5 ${hamburgerOpen ? 'show' : 'dontShow'}`}>
-                    <a href="#about">About</a>
-                    <a href="#projects">Projects</a>
-                    <a href="#technologies">Technologies</a>
+                <nav>
+                    <ul>
+                        <input type="checkbox" id="checkbox_toggle" className="hamburger-checkbox"/>
+                        <label htmlFor="checkbox_toggle" className="hamburger"><Hamburger size={30} color="#61dbe3" onToggle={toggleHamburger}/></label>
+                        <div className="landing-page-menu">
+                            <li><a href="#about" className="navigation-navbar-items">About</a></li>
+                            <li><a href="#projects" className="navigation-navbar-items">Projects</a></li>
+                            <li><a href="#technologies" className="navigation-navbar-items">Technologies</a></li>
+                        </div>
+                    </ul>
                 </nav>
-                <div className="mode-toggle mt-5 ">
+                <div className="mode-toggle  ">
                     <DarkModeToggle
                         mode={mode}
                         size="sm"
@@ -55,30 +58,28 @@ function Header({ isDarkMode, onModeToggle }) {
                         
                     />
                 </div>
-                <div className="links mt-5 d-flex flex-row gap-3">
-                    <a href="https://www.linkedin.com/in/lilifarkas" target="_blank" rel="noopener noreferrer"
-                       className="social-icons">
-                        <FontAwesomeIcon icon={faLinkedin} size="2x" color="#56A8C4"/>
-                    </a>
-                    <a href="https://github.com/lilifarkas" target="_blank" rel="noopener noreferrer" className="social-icons">
-                        <FontAwesomeIcon icon={faGithub} size="2x" color="#56A8C4"/>
-                    </a>
-                </div>
             </div>
             <div className="header-content">
-                <div className="header-text d-flex flex-column w-50">
-                    <div className="introduction">
-                        Hello, I'm <span className="rainbow-text">Lili Farkas</span>,<br /><br />
-                        Full-Stack Developer
+                <div className="header-text">
+                    <div className="main-introduction">
+                        <div className="introduction">
+                            Hello, I'm <span className="rainbow-text">Lili Farkas</span>,<br /><br />
+                            Full-Stack Developer
+                        </div>
+                        <br />
+                        <p className="intro">Along with my studies in health promotion, my extensive experience
+                            in both individual and collaborative work settings has given me a unique perspective that
+                            I bring to software development, to meet the specific requirements of each project.
+                            Experienced in connecting front-end design with backend infrastructure and possess a
+                            strong grasp of OOP concepts and techniques.</p>
                     </div>
-                    <br /><br />
-                    <p className="intro">Along with my studies in health promotion, my extensive experience 
-                        in both individual and collaborative work settings has given me a unique perspective that 
-                        I bring to software development, to meet the specific requirements of each project. 
-                        Experienced in connecting front-end design with backend infrastructure and possess a 
-                        strong grasp of OOP concepts and techniques.</p>
+                    <div className="profile">
+                        <img className="profile-picture" src={profilePicture} alt="Profile" />
+                    </div>
+                </div>
+                <div className="introduction-links">
                     <animated.button
-                        className="cv-button gradient-text"
+                        className="cv-button gradient-text align-self-end"
                         style={buttonAnimation}
                         onMouseEnter={() => setIsHovered(true)}
                         onMouseLeave={() => setIsHovered(false)}
@@ -86,9 +87,15 @@ function Header({ isDarkMode, onModeToggle }) {
                     >
                         Download CV
                     </animated.button>
-                </div>
-                <div className="profile align-self-start">
-                    <img className="profile-picture" src={profilePicture} alt="Profile" />
+                    <div className="links mt-5 d-flex flex-row align-items-center gap-3">
+                        <a href="https://www.linkedin.com/in/lilifarkas" target="_blank" rel="noopener noreferrer"
+                           className="social-icons align-self-start">
+                            <FontAwesomeIcon icon={faLinkedin} size="2x" color="#56A8C4"/>
+                        </a>
+                        <a href="https://github.com/lilifarkas" target="_blank" rel="noopener noreferrer" className="social-icons">
+                            <FontAwesomeIcon icon={faGithub} size="2x" color="#56A8C4"/>
+                        </a>
+                    </div>
                 </div>
             </div>
         </header>
