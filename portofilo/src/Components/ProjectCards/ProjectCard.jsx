728345b53e62e3289c193848fb97849be061670f
lili.farkas <lili.farkas14@gmail.com>
2023-07-17 22:26:25 +0200

    Add a download functionality to the project cards to download a console application

--- portofilo/src/Components/ProjectCards/ProjectCard.jsx
+++ portofilo/src/Components/ProjectCards/ProjectCard.jsx
@@ -1,25 +1,48 @@
 import React from 'react';
 import './ProjectCard.css';
+// import createZip from "./createZip";
+import MarsExplorer from "../MarsExplorer/MarsExplorer.rar";
+import FadeIn from '../Animation/FadeIn'
 
 
 function ProjectCard({ title, description, imageSrc, githubLink, previewLink}) {
+
+    const handleDownloadClick = () => {
+        const link = document.createElement('a');
+        link.href = MarsExplorer;
+        link.download = 'MarsExplorer.rar';
+        link.click();
+    };
+
+    const isLink = previewLink && previewLink.startsWith('http');
+    
     return (
         <div className="project-card ">
-            <div className="card-content">
-                <img src={imageSrc} alt={title} />
-                <div className="project-info">
-                    <h3>{title}</h3>
-                    <p className="project-description">{description}</p>
-                    <div className="button-container">
-                        <a href={githubLink} target="_blank" rel="noopener noreferrer" className="card-buttons">
-                            Check on GitHub
-                        </a>
-                        <a href={previewLink} target="_blank" rel="noopener noreferrer" className="card-buttons">
-                            Live preview
-                        </a>
+            <FadeIn>
+                <div className="card-content">
+                    <img src={imageSrc} alt={title} />
+                    <div className="project-info">
+                        <h3>{title}</h3>
+                        <div>
+                            <p className="project-description">{description}</p>
+                        </div>
+                        <div className="button-container">
+                            <a href={githubLink} target="_blank" rel="noopener noreferrer" className="card-buttons">
+                                Check on GitHub
+                            </a>
+                            {isLink ? (
+                                <a href={previewLink} target="_blank" rel="noopener noreferrer" className="card-buttons">
+                                    Live preview
+                                </a>
+                            ) : (
+                                <button className="card-buttons" onClick={handleDownloadClick}>
+                                    Download
+                                </button>
+                            )}
+                        </div>
                     </div>
                 </div>
-            </div>
+            </FadeIn>
         </div>
     );
 }
